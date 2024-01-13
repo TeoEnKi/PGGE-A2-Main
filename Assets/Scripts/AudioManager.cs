eff7d6bd77be0cdfd1fdc884550441872e7a963a
TeoEnKi <teoenki@gmail.com>
2024-01-13 14:20:26 +0800

    adding audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void PlaySingleplayerBtnSound()
+    {
+
+    }
+    void PlayMultiplayerBtnSound()
+    {
+
+    }
+    void PlayJoinRoomSound()
+    {
+
+    }
+    void PlayReturnMenuSound()
+    {
+
+    }
+}
